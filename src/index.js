4e3fd2ac8590531bbd7a6fcc6314b77290d0064f
PranjeetGoswami <prgoswami2020@gmail.com>
2025-02-17 00:01:50 +0530

    Created src manually, added all the assets and librarirs, created all the components and pages and styles

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { ContextProvider } from "./contexts/ContextProvider";
+
+import App from "./App";
+
+import { createRoot } from "react-dom/client";
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
+  <ContextProvider>
+    <App />
+  </ContextProvider>
+);
