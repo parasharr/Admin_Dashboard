e3efc3e86b05db2b070a9f88fe02d9b89ed606a4
PranjeetGoswami <prgoswami2020@gmail.com>
2025-02-17 22:02:29 +0530

    added sparkling and stacked chart from syncfusion

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -18,7 +18,7 @@ const Ecommerce = () => {
           <div className="flex justify-between items-center">
             <div>
               <p className="font-bold text-gray-400">Earnings</p>
-              <p className="text-2xl ">$63,448.79</p>
+              <p className="text-2xl text-gray-600 font-semibold">$63,448.79</p>
             </div>
           </div>
           <div className="mt-6">
@@ -33,28 +33,25 @@ const Ecommerce = () => {
         </div>
 
         <div className="flex m-3 flex-wrap justify-center gap-1 items-center">
-          {earningData.map((item)=> (
-            <div 
-            className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56 p-4 pt-9 rounded-2xl" 
-            key={item.title}>
-              <button type="button" style={{color: item.iconColor,
-                backgroundColor: item.iconBg
-              }}
-              className="text-2xl opacity-0.9 rounded-full p-4 hover:drop-shadow-xl"
+          {earningData.map((item) => (
+            <div
+              className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56 p-4 pt-9 rounded-2xl"
+              key={item.title}
+            >
+              <button
+                type="button"
+                style={{ color: item.iconColor, backgroundColor: item.iconBg }}
+                className="text-2xl opacity-0.9 rounded-full p-4 hover:drop-shadow-xl"
               >
                 {item.icon}
               </button>
               <p className="mt-3">
-                <span className="text-lg font-semibold">
-                  {item.amount}
-                </span>
+                <span className="text-lg font-semibold">{item.amount}</span>
                 <span className={`text-sm text-${item.pcColor} ml-2`}>
                   {item.percentage}
                 </span>
               </p>
-              <p className="text-sm text-gray-400 mt-1">
-                {item.title}
-              </p>
+              <p className="text-sm text-gray-400 mt-1">{item.title}</p>
             </div>
           ))}
         </div>
@@ -66,9 +63,61 @@ const Ecommerce = () => {
             <p className="font-semibold text-xl">Revenue Updates</p>
             <div className="flex items-center gap-4">
               <p className="flex items-center gap-2 text-gray-600 hover:drop-shadow-xl">
-                <span><GoDotFill /></span>
+                <span>
+                  <GoDotFill />
+                </span>
                 <span>Expense</span>
               </p>
+              <p className="flex items-center gap-2 text-green-600 hover:drop-shadow-xl">
+                <span>
+                  <GoDotFill />
+                </span>
+                <span>Budget</span>
+              </p>
+            </div>
+          </div>
+
+          <div className="mt-10 flex gap-10 flex-wrap justify-center">
+            <div className="border-r-1 border-color m-4 pr-10">
+              <div>
+                <p>
+                  <span className="text-3xl font-semibold">$93,453</span>
+                  <span className="p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-400 ml-3 text-xs">
+                    24%
+                  </span>
+                </p>
+                <p className="text-gray-500 mt-1">Budget</p>
+              </div>
+              <div className="mt-8">
+                <p>
+                  <span className="text-3xl font-semibold">$49,453</span>
+                </p>
+                <p className="text-gray-500 mt-1">Expense</p>
+              </div>
+
+              <div className="mt-5">
+                <SparkLine
+                  currentColor="blue"
+                  id="line-sparkline"
+                  type="Line"
+                  height="80px"
+                  width="250px"
+                  data={SparklineAreaData}
+                  color="blue"
+                />
+              </div>
+              <div className="mt-10">
+                <Button
+                  color="white"
+                  bgColor="blue"
+                  text="Download Report"
+                  borderRadius="10px"
+                />
+              </div>
+            </div>
+
+            <div>
+              <Stacked width="320px" height="360px" />
             </div>
           </div>
         </div>
