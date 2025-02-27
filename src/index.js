cbcc160c045928da3c5dfd66ef776eb97ba83ee5
PranjeetGoswami <prgoswami2020@gmail.com>
2025-02-27 21:26:13 +0530

    fixed minor errors

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from "react";
-import "./index.css";
-import { ContextProvider } from "./contexts/ContextProvider";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import App from "./App";
+import './index.css';
+import App from './App';
+import { ContextProvider } from './contexts/ContextProvider';
 
-import { createRoot } from "react-dom/client";
-
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(
-  <ContextProvider>
-    <App />
-  </ContextProvider>
+ReactDOM.render(
+  <React.StrictMode>
+    <ContextProvider>
+      <App />
+    </ContextProvider>
+  </React.StrictMode>,
+  document.getElementById('root'),
 );
